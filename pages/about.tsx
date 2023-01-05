609df170489adb1b572a09b7b136afefa4483201
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-01-05 20:06:58 +0900

    add icon

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/alt-text */
 import type { NextPage } from 'next';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
-  return <>About</>;
+  return (
+    <>
+      <div className="about">
+        <Image src="/metamong.jpg" width="120px" height="120px"></Image>
+        <div style={{ marginTop: '20px' }}>Hello. I'm a student living in Korea.</div>
+        <div>
+          <a href="https://github.com/do4ng">github</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
