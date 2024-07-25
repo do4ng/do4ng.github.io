e7c7c8d9ef1e9b150bb879d44ad2bd1e2ecaa761
r.ehdals <57896501+do4ng@users.noreply.github.com>
2024-07-25 16:38:31 +0900

    fix: project page

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -4,44 +4,43 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import { Content } from '../../mdx/content';
 
-interface RepoType {
-  full_name: string;
-  html_url: string;
-  description?: string;
-  stargazers_count: number;
-  language: string;
-  homepage?: string;
-  fork: boolean;
+export async function getStaticProps(ctx: GetStaticPropsContext) {
+  let markdown;
+  if (process.env.NODE_ENV === 'development') {
+    markdown = readFileSync(join(process.cwd(), 'posts', `${'0-hello'}.mdx`)).toString();
+  } else {
+    try {
+      const res = await fetch(
+        `https://raw.githubusercontent.com/do4ng/do4ng.github.io/main/posts/${'0-hello'}.mdx`
+      );
+      markdown = await res.text();
+    } catch (e) {
+      console.error(e);
+      return {
+        props: {
+          markdown: null,
+          reason: e,
+        },
+      };
+    }
+  }
+
+  const compiled = await compileMdx(markdown);
+
+  return {
+    props: {
+      compiled,
+    },
+  };
 }
 
-export default function Repos({ repos, compiled }: { repos: RepoType[]; compiled: any }) {
+export default function Repos({ compiled }: { compiled: any }) {
   return (
     <>
       <h1 style={{ marginTop: '125px', paddingLeft: '10px' }}>Projects</h1>
       <div className="hello">
         <p className="title">/0-hello.mdx</p>
         <Content content={compiled}></Content>
-      </div>
-      <div className="repos">
-        {repos.map((repo) => (
-          <>
-            <div className="repo">
-              <h3>
-                <a href={repo.html_url}>{repo.full_name}</a>
-              </h3>
-              <p>{repo.description || 'No description'}</p>
-              <div className="info">
-                <div className="star">
-                  <i className="ri-star-line"></i>
-                  <p>{repo.stargazers_count}</p>
-                </div>
-                <div className={`language ${`language-${repo.language.toLowerCase()}`}`}>
-                  {repo.language}
-                </div>
-              </div>
-            </div>
-          </>
-        ))}
       </div>
     </>
   );
